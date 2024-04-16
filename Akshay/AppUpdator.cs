bf62eee151e4e5bd6db47775b50d2750e2b29dc9
Akskmr1234 <9645424@gmail.com>
2024-04-16 17:24:00 +0530

    Update

--- Akshay/AppUpdator.cs
+++ Akshay/AppUpdator.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
 
 namespace CsHms.Akshay
 {
@@ -14,26 +15,43 @@ namespace CsHms.Akshay
         public AppUpdator()
         {
             InitializeComponent();
-            ExecuteGitCommand("fetch");
+            
         }
-        private void ExecuteGitCommand(string command)
+        private void ExecuteGitCommand()
         {
             try
             {
-                ProcessStartInfo StartInfo = new ProcessStartInfo();
-                StartInfo.FileName = "cmd.exe";
-                StartInfo.Arguments = "/c " + command;
-                StartInfo.RedirectStandardInput = true;
-                StartInfo.RedirectStandardOutput = true;
-                StartInfo.UseShellExecute = false;
-                StartInfo.CreateNoWindow = true;
-                Process process = new Process();
-                process.StartInfo = StartInfo;
-                process.Start();
-                string output = process.StandardOutput.ReadToEnd();
-                txtOutput.Text = output;
-                process.WaitForExit();
-                MessageBox.Show("Updated");
+                // Disable the button while the command is executing
+                btnUpdate.Enabled = false;
+
+                // Clear the output textbox
+                txtOutput.Text = "";
+                string ParentFolder = Directory.GetParent(Application.StartupPath).FullName;
+                // Start a new process to execute Git commands
+                Process gitProcess = new Process();
+                gitProcess.StartInfo.FileName = "git";
+                gitProcess.StartInfo.WorkingDirectory = ParentFolder;
+                gitProcess.StartInfo.Arguments = "fetch";
+                gitProcess.StartInfo.UseShellExecute = false;
+                gitProcess.StartInfo.RedirectStandardOutput = true;
+                gitProcess.StartInfo.RedirectStandardError = true;
+                gitProcess.OutputDataReceived += GitOutputHandler;
+                gitProcess.ErrorDataReceived += GitOutputHandler;
+                gitProcess.Start();
+                gitProcess.BeginOutputReadLine();
+                gitProcess.BeginErrorReadLine();
+                gitProcess.WaitForExit();
+
+                // Pull changes after fetching
+                gitProcess.StartInfo.Arguments = "pull";
+                gitProcess.Start();
+                gitProcess.BeginOutputReadLine();
+                gitProcess.BeginErrorReadLine();
+                gitProcess.WaitForExit();
+
+                // Re-enable the button after command execution
+                btnUpdate.Enabled = true;
+                
             }
             catch (Exception ex)
             { MessageBox.Show(ex.Message.ToString()); }
@@ -41,7 +59,19 @@ namespace CsHms.Akshay
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            ExecuteGitCommand("pull");
+            ExecuteGitCommand();
+            MessageBox.Show("Updated");
+        }
+
+        
+        // Method to handle Git command output
+        private void GitOutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+                txtOutput.AppendText(e.Data + Environment.NewLine);
+
+            }
         }
 
        
